b82f5309d7bc8848ad533b47a016b2fe135aa410
Bernardo Moraes <bernardo.moraes.silva@gmail.com>
2020-04-18 17:57:37 -0300

    Primeiros Exercícios de Python

---
+++ Python Exercises/Acompanhando a aula/Aula 01 Desafio 03.py
@@ -0,0 +1,7 @@
+
+n1= input ('Número 1')
+n2= input ('Número 2')
+
+int = n1,n2
+print (n1+n2)
+
---
+++ Python Exercises/Acompanhando a aula/Desafio 02 aula 1.py
@@ -0,0 +1,4 @@
+dia= input ('Qual o dia que você nasceu? ')
+mes= input('Qual o mes que você nasceu? ')
+ano= input ('Qual o ano que você nasceu? ')
+print (" você nasceu no dia", dia, "do mês", mes, "de", ano, '. Correto?')
