19de1cecf45747604891870f06a53b890bd266c7
Bernardo Moraes <bernardo.moraes.silva@gmail.com>
2020-04-19 23:42:24 -0300

    Adicionando exercícios de python D1

---
+++ Python Exercises/ex000.py
@@ -0,0 +1,8 @@
+n1= int(input('Digite um número'))
+n2= int(input('digite outro número'))
+# print(n1+n2)
+s = n1 + n2
+print('A soma entre {} e {} é igual a {}. É nois meu parça!'.format (n1,n2,s))
+
+
+
